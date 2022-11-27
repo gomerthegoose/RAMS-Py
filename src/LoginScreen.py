4fea36be34982236c9f66a0f65ab0f32e62b72e3
gomerthegoose <gomerthegoose@gmail.com>
2022-11-27 09:36:16 +0000

    login screen

--- src/LoginScreen.py
+++ src/LoginScreen.py
@@ -4,15 +4,15 @@ import customtkinter
 
 class LoginScreen(customtkinter.CTk):
     
-    defaultHeight = 200
-    defaultWidth = 300
+    defaultHeight = 150
+    defaultWidth = 250
     
     def __init__(self):
         super().__init__()
         
         
         
-        self.title("CustomTkinter complex_example.py")
+        self.title("Login")
         self.geometry(f"{LoginScreen.defaultWidth}x{LoginScreen.defaultHeight}")
         #self.protocol("WM_DELETE_WINDOW", self.on_closing)  # call .on_closing() when app gets closed
         
@@ -22,20 +22,20 @@ class LoginScreen(customtkinter.CTk):
 
         #create frame to contain entry box and button
         self.Login_Frame = customtkinter.CTkFrame(master=self)
-        self.Login_Frame.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+        self.Login_Frame.grid(row=0, column=1, sticky="nswe", padx=10 , pady=10)
         
-        
+              
         # - userName entry field -  
         self.username_Entry = customtkinter.CTkEntry(master=self.Login_Frame,
-                                            width=120,
+                                            width=200,
                                             placeholder_text="Usernmae")
-        self.username_Entry.place(relx=0.5, rely=0.5, anchor=customtkinter.top)
+        self.username_Entry.place(relx=0.5, rely=0.2, anchor=customtkinter.CENTER)
         
         # - Password entry field - 
         self.password_Entry = customtkinter.CTkEntry(master=self.Login_Frame,
-                                            width=120,
+                                            width=200,
                                             placeholder_text="Password")
-        self.password_Entry.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+        self.password_Entry.place(relx=0.5, rely=0.45, anchor=customtkinter.CENTER)
         
         
         # - login Button -
@@ -44,7 +44,7 @@ class LoginScreen(customtkinter.CTk):
                                                 border_width=2,  # <- custom border_width
                                                 fg_color=None,  # <- no fg_color
                                                 command=self.LoginButtonHandle)
-        self.login_Button.place(relx=0.5, rely=0.5, anchor=customtkinter.BOTTOM)
+        self.login_Button.place(relx=0.5, rely=0.8, anchor=customtkinter.CENTER)
         
         
         
