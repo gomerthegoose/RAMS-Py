e292f2fcc80fe2386decab256e6cbaa8c72b7fb8
gomerthegoose <gomerthegoose@gmail.com>
2022-12-01 21:07:28 +0000

    python is suffering

--- src/MainMenu.py
+++ src/MainMenu.py
@@ -1,130 +1,58 @@
-import tkinter
-from tkinter import messagebox as msg
-import customtkinter
-import StaffMenu
+import customtkinter as CTK
 
-
-
-class MainMenu(customtkinter.CTk):
-    
-    
-    defaultWindowHeight = 1000
-    defaultWindowWidth = 1000
-    controlsWidth = 190
-
-    
+class App(CTK.CTk):
     def __init__(self):
         super().__init__()
-             
-        self.title("Main Menu")
-        self.geometry(f"{MainMenu.defaultWindowWidth}x{MainMenu.defaultWindowHeight}")
-               
-        # configure grid layout (2x1)
-        self.grid_columnconfigure(2, weight=1)
+
+        self.title("image_example.py")
+        self.geometry("700x450")
+
+        # set grid layout 1x2
         self.grid_rowconfigure(0, weight=1)
-        
-        
+        self.grid_columnconfigure(1, weight=1)
 
-        #create frame to contain entry box and button
-        self.Controls_Frame = customtkinter.CTkFrame(master=self)
-        self.Controls_Frame.grid(row=0, column=1, sticky="nsw", padx=10 , pady=10)
-        
-        self.View_Frame = customtkinter.CTkFrame(master=self)
-        self.View_Frame.grid(row=0,column=2,sticky="nswe",padx=10,pady=10)
-
-        # - frame used to parent buttons for main menu -
-        self.MainMenuControls_Frame = customtkinter.CTkFrame(master=self.Controls_Frame,border_color="default_theme",border_width="default_theme")
-        self.MainMenuControls_Frame.grid(row=0,column=2,sticky="nwe",padx=2,pady=2)
-        
-        
-        #create buttons for controls frame 
-        
-        self.quit_button = customtkinter.CTkButton(master=self.Controls_Frame,
-                                                    text="Exit",
-                                                    border_width=2,  # <- custom border_width
-                                                    width=self.controlsWidth,
-                                                    command=self.quitApplication,
-                                                    fg_color="red",
-                                                    hover_color="darkRed")
-        self.quit_button.place(relx=0.5, rely=0.99, anchor=customtkinter.S)
-        
-        self.staff_button = customtkinter.CTkButton(master=self.MainMenuControls_Frame,
-                                                    text="Staff",
-                                                    border_width=2,  # <- custom border_width
-                                                    width=self.controlsWidth,
-                                                    command=self.openStaffMenu)
-        self.staff_button.place(relx=0.5, rely=0.15, anchor=customtkinter.N)
-        
-        self.stock_button = customtkinter.CTkButton(master=self.MainMenuControls_Frame,
-                                                    text="Stock",
-                                                    border_width=2,  # <- custom border_width
-                                                    width=self.controlsWidth,
-                                                    command=self.openStockMenu)
-        self.stock_button.place(relx=0.5, rely=0.30, anchor=customtkinter.N)
-        
-        self.thursdayBook_button = customtkinter.CTkButton(master=self.MainMenuControls_Frame,
-                                                    text="Thursday Bookings",
-                                                    border_width=2,  # <- custom border_width
-                                                    width=self.controlsWidth,
-                                                    command=self.openThursdayBookingsMenu)
-        self.thursdayBook_button.place(relx=0.5, rely=0.45, anchor=customtkinter.N)
-        
-        self.sundayBook_button = customtkinter.CTkButton(master=self.MainMenuControls_Frame,
-                                                    text="Sunday Bookings",
-                                                    border_width=2,  # <- custom border_width
-                                                    width=self.controlsWidth,
-                                                    command=self.openSundayBookingsMenu)
-        self.sundayBook_button.place(relx=0.5, rely=0.60, anchor=customtkinter.N)     
-        
-        
-        # create label for controls frame
-        
-        self.controls_label = customtkinter.CTkLabel(master=self.MainMenuControls_Frame, text="- Options -", text_font = ("",18))
-        self.controls_label.place(relx=0.5, rely=0.06, anchor=tkinter.CENTER)
-        
-    # - exit application -    
-    def quitApplication(self):
-        print ("totaly functional")
-        
-        
-        
-        
-    # - open new sub menu -
-    def openStockMenu(self):
-        print("Opening Menu: Stock")
-        self.MainMenuControls_Frame.lower()
+        # load images with light and dark mode image
+        # create navigation frame
+        self.navigation_frame = CTK.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
 
 
-    def openStaffMenu(self):
-        print("Opening Menu: Staff")
-        self.MainMenuControls_Frame.lower()
-        staffMenu = StaffMenu.StaffMenu()
-        
-        # - Create staff Menu Widgets -    
-        self.StaffMenuControls_Frame = customtkinter.CTkFrame(master=self.Controls_Frame,border_color="default_theme",border_width="default_theme")
-        self.StaffMenuControls_Frame.grid(row=0,column=2,sticky="nwe",padx=2,pady=2)
-               
-        self.ViewStaff_button = customtkinter.CTkButton(master=self.StaffMenuControls_Frame,
-                                                    text="View Staff",
-                                                    border_width=2,  # <- custom border_width
-                                                    width=self.controlsWidth,
-                                                    command=self.openSundayBookingsMenu)
-        self.ViewStaff_button.place(relx=0.5, rely=0.60, anchor=customtkinter.N)
-        
-        staffMenu.setActiveMenu(test=self.Controls_Frame)
-        
-        
-        
-        
-        
-            
-    def openSundayBookingsMenu(self):
-        print("Opening Menu: Sunday Bookings")
-        self.MainMenuControls_Frame.lower()
-        
-    def openThursdayBookingsMenu(self):
-        print("Opening Menu: Thursday Bookings")
-        self.MainMenuControls_Frame.lower()
-        
-        
-        
+
+
+
+
+
+
+        self.select_frame_by_name("home")
+
+
+
+    def select_frame_by_name(self, name):
+        # show selected frame
+        if name == "home":
+            self.home_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.home_frame.grid_forget()
+        if name == "frame_2":
+            self.second_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.second_frame.grid_forget()
+        if name == "frame_3":
+            self.third_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.third_frame.grid_forget()
+
+    def home_button_event(self):
+        self.select_frame_by_name("home")
+
+    def frame_2_button_event(self):
+        self.select_frame_by_name("frame_2")
+
+    def frame_3_button_event(self):
+        self.select_frame_by_name("frame_3")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
--- test.py
+++ test.py
@@ -1,30 +1,91 @@
-# Import the required libraries
-from tkinter import *
-from tkinter import ttk
+import customtkinter as CTK
 
-# Create an instance of tkinter frame
-win = Tk()
+class App(CTK.CTk):
+    def __init__(self):
+        super().__init__()
 
-# Set the size of the tkinter window
-win.geometry("700x350")
+        self.title("image_example.py")
+        self.geometry("700x450")
 
-# Define the style for combobox widget
-style = ttk.Style()
-style.theme_use('xpnative')
+        # set grid layout 1x2
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
 
-# Define a function to show/hide widget
-def show_widget():
-   label.pack()
-def hide_widget():
-   label.pack_forget()
-   b1.configure(text="Show", command=show_widget)
+        # load images with light and dark mode image
+        # create navigation frame
+        self.navigation_frame = CTK.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
 
-# Add a label widget
-label = ttk.Label(win, text="Eat, Sleep, Code and Repeat", font=('Aerial 11'))
-label.pack(pady=30)
+        self.home_button = CTK.CTkButton(self.navigation_frame, corner_radius=0, height=40, text="Home",
+                                                    text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                     command=self.home_button_event)
+        self.home_button.grid(row=1, column=0, sticky="ew")
 
-# Add a Button widget
-b1 = ttk.Button(win, text="Hide", command=hide_widget)
-b1.pack(pady=20)
 
-win.mainloop()
+        
+        self.frame_2_button = CTK.CTkButton(self.navigation_frame, corner_radius=0, height=40, text="Frame 2",
+                                                       text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                        command=self.frame_2_button_event)
+        self.frame_2_button.grid(row=2, column=0, sticky="ew")
+        
+
+        
+        self.frame_3_button = CTK.CTkButton(self.navigation_frame, corner_radius=0, height=40, text="Frame 3",
+                                                       text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                        command=self.frame_3_button_event)
+        self.frame_3_button.grid(row=3, column=0, sticky="ew")
+        
+
+
+        # create home frame
+        self.home_frame = CTK.CTkFrame(self, corner_radius=0)
+        self.home_frame.grid_columnconfigure(0, weight=1)
+
+
+        # create second frame
+        self.second_frame = CTK.CTkFrame(self, corner_radius=0)
+
+        # create third frame
+        self.third_frame = CTK.CTkFrame(self, corner_radius=0)
+        
+        self.controls_label = CTK.CTkLabel(master=self.home_frame, text="- home -", text_font = ("",18))
+        self.controls_label.place(relx=0.5, rely=0.5, anchor=CTK.CENTER)
+        self.controls_label = CTK.CTkLabel(master=self.second_frame, text="- first -", text_font = ("",18))
+        self.controls_label.place(relx=0.5, rely=0.5, anchor=CTK.CENTER)
+        self.controls_label = CTK.CTkLabel(master=self.third_frame, text="second", text_font = ("",18))
+        self.controls_label.place(relx=0.5, rely=0.5, anchor=CTK.CENTER)
+        # select default frame
+        self.select_frame_by_name("home")
+
+    def select_frame_by_name(self, name):
+        # set button color for selected button
+
+
+        # show selected frame
+        if name == "home":
+            self.home_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.home_frame.grid_forget()
+        if name == "frame_2":
+            self.second_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.second_frame.grid_forget()
+        if name == "frame_3":
+            self.third_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.third_frame.grid_forget()
+
+    def home_button_event(self):
+        self.select_frame_by_name("home")
+
+    def frame_2_button_event(self):
+        self.select_frame_by_name("frame_2")
+
+    def frame_3_button_event(self):
+        self.select_frame_by_name("frame_3")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
