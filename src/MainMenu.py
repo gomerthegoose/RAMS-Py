618166853868abeb5d30f7f43995f05c6e095c3e
Gomer <gomerthegoose@gmail.com>
2022-12-02 16:24:04 +0000

    menu?

--- src/MainMenu.py
+++ src/MainMenu.py
@@ -1,58 +1,45 @@
-import customtkinter as CTK
+import customtkinter
 
-class App(CTK.CTk):
+
+class MainMenu(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+        self.title("Main Menu")
+        self.geometry("1000,800")
 
-        self.title("image_example.py")
-        self.geometry("700x450")
-
-        # set grid layout 1x2
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
+        mainFrame = customtkinter.CTkFrame()
+        mainFrame.grid(row=0,column=0,sticky="nsew")
+        
+        
+        self.pages = {}
+        for page in (MainMenuPage,testPage):
+            pageName = page.__name__
+            frame = page(MainFrame=mainFrame)
+            self.pages[pageName] = frame
+
+    def openPage(self,pageName):
+        self.pages[pageName].grid(row=0,column=0,sticky="nsew")
+
+
+
+class MainMenuPage(customtkinter.CTkFrame):
+    def __init__(self,MainFrame):
         # load images with light and dark mode image
         # create navigation frame
-        self.navigation_frame = CTK.CTkFrame(self, corner_radius=0)
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
         self.navigation_frame.grid_rowconfigure(4, weight=1)
 
-
-
-
-
-
-
-
-        self.select_frame_by_name("home")
-
-
-
-    def select_frame_by_name(self, name):
-        # show selected frame
-        if name == "home":
-            self.home_frame.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.home_frame.grid_forget()
-        if name == "frame_2":
-            self.second_frame.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.second_frame.grid_forget()
-        if name == "frame_3":
-            self.third_frame.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.third_frame.grid_forget()
+        self.home_button = customtkinter.CTkButton(self.navigation_frame, text="Home",
+                                                     command=self.home_button_event)
+        self.home_button.grid(row=1, column=1, sticky="ew")
 
     def home_button_event(self):
-        self.select_frame_by_name("home")
+        print("fully functional")
 
-    def frame_2_button_event(self):
-        self.select_frame_by_name("frame_2")
-
-    def frame_3_button_event(self):
-        self.select_frame_by_name("frame_3")
-
-
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+class testPage(customtkinter.CTkFrame):
+    def __init__(self,MainFrame):
+        pass
